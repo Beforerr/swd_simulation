8b2a52c2f11ee8679ef7c770327c60f4edf265be
Beforerr <zzj956959688@gmail.com>
2024-04-26 20:28:53 -0700

    add wk and  power_spectrum plot

--- warpx/utils/__init__.py
+++ warpx/utils/__init__.py
@@ -1,8 +1,21 @@
-import numpy as np
+from yt.data_objects.time_series import SimulationTimeSeries
+from yt.data_objects.static_output import Dataset
+import pandas as pd
 
-def cosd(x):
-    return np.cos(np.deg2rad(x))
+def ds2df(ds: Dataset, type = "particle", coords = None) -> pd.DataFrame:
+    if type == "particle":
+        fields = ds.field_list
+    elif type == "field":
+        coords = coords or ["x", "y", "z"]
+        fields = ds.field_list + coords
+    return ds.all_data().to_dataframe(fields).assign(time = ds.current_time)
 
+def export_ds(ds: Dataset, **kwargs):
+    # new_fn = ds.filename(ext, "arrow")
+    new_fn = ds.filename + ".arrow"
+    df = ds2df(ds, **kwargs)
+    df.to_feather(new_fn)
+    return new_fn
 
-def sind(x):
-    return np.sin(np.deg2rad(x))
+def export_ts(ts: SimulationTimeSeries,  **kwargs):
+    return [export_ds(ds, **kwargs) for ds in ts.piter()]
